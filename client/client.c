5634c6f7a23c10f389e7f3213a4d3eeef4fa63cb
igorgardzielewski <igorgardzielewski3@gmail.com>
2025-04-01 17:12:04 +0200

    first commit

---
+++ client/client.c
@@ -0,0 +1,207 @@
+#define CONFIG_FILE "client_config.txt"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <math.h>
+#include <pthread.h>
+#include <errno.h>
+//te definy sa zle popraw je bo sa takie same
+#define SQRT_REQUEST 0x01
+#define TIME_REQUEST 0x02
+#define SQRT_RESPONSE 0x01
+#define TIME_RESPONSE 0x02
+#define BUFFER_SIZE 1024
+typedef struct
+{
+    int socket;
+    char server_ip[16];
+    long int server_port;
+}ConnectionInfo;
+void empty3(uint8_t buff[])
+{
+    buff[0] = 0x00;
+    buff[1] = 0x00;
+    buff[2] = 0x00;
+}
+void sqrt_req(ConnectionInfo *conn, uint8_t rq_id, double num)
+{
+    if(num<0)
+    {
+        printf("5. Invalid request\n");
+        return;
+    }
+    uint64_t network_num;
+    memcpy(&network_num, &num, sizeof(uint64_t));
+    //host to big endian z little endian lub big endian do host
+    network_num = htobe64(network_num);
+    uint8_t request[13];
+    empty3(request);
+    request[3] = SQRT_REQUEST;
+    request[4] = rq_id;
+    memcpy(&request[5], &network_num, sizeof(uint64_t));
+    write(conn->socket, request, 13);
+}
+void time_req(ConnectionInfo *conn, uint8_t rq_id)
+{
+    uint8_t request[5];
+    empty3(request);
+    request[3] = TIME_REQUEST;
+    request[4] = rq_id;
+    write(conn->socket, request, 5);
+}
+void response(ConnectionInfo *conn)
+{
+    //musi byc buffer bo na 2 redy podzielone nie dziala
+    uint8_t buff[BUFFER_SIZE];
+    ssize_t bytes_read = read(conn->socket, buff, BUFFER_SIZE);
+    if(bytes_read < 6)
+    {
+        printf("5. Invalid response\n");
+        return;
+    }
+
+    size_t offset = 0;
+    while(offset < bytes_read)
+    {
+        uint8_t isResponse = buff[offset];
+        if(isResponse != 0x01)
+        {
+            printf("5. Invalid response\n");
+            return;
+        }
+
+        uint8_t response_Type = buff[offset + 3];
+        uint8_t rq_id = buff[offset + 4];
+
+        switch(response_Type)
+        {
+            case SQRT_RESPONSE:
+            {
+                uint64_t result;
+                memcpy(&result, &buff[offset + 5], sizeof(uint64_t));
+                result = be64toh(result);
+                double res;
+                memcpy(&res, &result, sizeof(double));
+                printf("--------\n");
+                printf("Response type: SQRT_RESPONSE\n");
+                printf("Request ID: %d\n", rq_id);
+                printf("Result: %lf\n", res);
+                printf("--------\n");
+                offset += 13;
+                break;
+            }
+            case TIME_RESPONSE:
+            {
+                uint16_t time_len;
+                memcpy(&time_len, &buff[offset + 5], sizeof(uint8_t));
+                time_len = time_len;
+                char time_str[32];
+                memcpy(time_str, &buff[offset + 6], time_len);
+                time_str[time_len] = '\0';
+                printf("--------\n");
+                printf("Response type: TIME_RESPONSE\n");
+                printf("Request ID: %d\n", rq_id);
+                printf("Time: %s\n", time_str);
+                printf("--------\n");
+                offset += 6 + time_len;
+                break;
+            }
+            default:
+            {
+                printf("5. Invalid response\n");
+                return;
+            }
+        }
+    }
+}
+//read config file
+//ATTRIBUTE:VALUE
+//ex SERVER_IP:127.0.0.1
+//ex SERVER_PORT:9090
+void parameters_read(ConnectionInfo *conn)
+{
+    FILE *config_file = fopen(CONFIG_FILE, "r");
+    if(config_file == NULL)
+    {
+        printf("1. Config file not found\n");
+        exit(1);
+    }
+    char line[256];
+    while(fgets(line, sizeof(line), config_file))
+    {
+        char *key = strtok(line, ":");
+        char *value = strtok(NULL, ":");
+        if(strcmp(key, "SERVER_IP") == 0)
+        {
+            value[strcspn(value, "\n")] = 0;
+            strcpy(conn->server_ip, value);
+        }
+        else if(strcmp(key, "SERVER_PORT") == 0)
+        {
+            conn->server_port = atol(value);
+        }
+    }
+    printf("1. Config file read successfully\n");
+    fclose(config_file);
+}
+int set_up_connection(ConnectionInfo *conn)
+{
+    parameters_read(conn);
+    //IPV4, TCP, 0 -- default tcp
+    conn->socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(conn->socket == -1)
+    {
+        printf("2. Socket creation failed\n");
+        return -1;
+    }
+    else
+    {
+        printf("2. Socket created successfully\n");
+    }
+    struct sockaddr_in server_address;
+        //struct ipv4
+    server_address.sin_family = AF_INET;
+        //htons do big endian
+
+    server_address.sin_port = htons(conn->server_port);
+        //konwertuje adres z postaci string na binarna
+
+    if(inet_pton(AF_INET, conn->server_ip, &server_address.sin_addr)<=0)
+    {
+        printf("3. Invalid address\n");
+        return -1;
+    }
+    printf("3. Address is valid\n");
+    if(connect(conn->socket, (struct sockaddr *)&server_address, sizeof(server_address))<0)
+    {
+        printf("4. Connection failed\n");
+        return -1;
+    }
+    else
+    {
+        printf("4. Connection established\n");
+    }
+    return 0;
+}
+int main()
+{
+    ConnectionInfo conn;
+    if(set_up_connection(&conn) == -1)
+    {
+        return -1;
+    }
+    //requst
+    sqrt_req(&conn,1, 16.0);
+    response(&conn);
+    time_req(&conn, 2);
+    response(&conn);
+    sqrt_req(&conn, 3, 25.0);
+    response(&conn);
+    close(conn.socket);
+    return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <math.h>
+#include <pthread.h>
+#include <errno.h>
+
+#define SQRT_REQUEST 0x01
+#define TIME_REQUEST 0x02
+#define SQRT_RESPONSE 0x01
+#define TIME_RESPONSE 0x02
+#define BUFFER_SIZE 1024
+#define MAX_CLIENT_THREADS 16
+pthread_mutex_t thread_count_mutex = PTHREAD_MUTEX_INITIALIZER;
+int thread_count = 0;
+void parameters_read(long int *SERVER_PORT)
+{
+    FILE *config_file = fopen("server_config.txt", "r");
+    if(config_file == NULL)
+    {
+        printf("1. Config file not found\n");
+        exit(1);
+    }
+    char line[256];
+    fgets(line, sizeof(line), config_file);
+    char *key = strtok(line, ":");
+    char *value = strtok(NULL, ":");
+    if(strcmp(key, "SERVER_PORT") == 0)
+    {
+        *SERVER_PORT = atol(value);
+    }
+    printf("1. Config file read successfully\n");
+    fclose(config_file);
+}
+typedef struct
+{
+    int socket_fd;
+    struct sockaddr_in client_address;
+}Client_t;
+void setArrayToResponse(uint8_t *res, uint8_t rq_id, uint8_t response_type)
+{
+    res[0] = 0x01;
+    res[1] = 0x00;
+    res[2] = 0x00;
+    res[3] = response_type;
+    res[4] = rq_id;
+}
+void sqrt_request(int client_socket, uint8_t rq_id, const uint8_t *buff)
+{
+    uint64_t network_num;
+    memcpy(&network_num, buff, sizeof(uint64_t));
+    network_num = be64toh(network_num);
+    double num;
+    memcpy(&num, &network_num, sizeof(double));
+    printf("%lf\n -> ",num);
+    double result = sqrt(num);
+    printf("%lf\n", result);
+    uint64_t network_result;
+    memcpy(&network_result, &result, sizeof(uint64_t));
+    network_result = htobe64(network_result);
+    uint8_t res[13];
+    setArrayToResponse(res, rq_id, SQRT_RESPONSE);
+    memcpy(&res[5], &network_result, sizeof(uint64_t));
+    write(client_socket, res, 13);
+}
+void time_request(int client_socket, uint8_t rq_id)
+{
+    time_t curr = time(NULL);
+    struct tm *time_info = localtime(&curr);
+    char time_str[32];
+    size_t tim_len=strftime(time_str, 32, "%Y-%m-%d %H:%M:%S", time_info);
+    printf("%s\n", time_str);
+    uint16_t network_tim_len = htons(tim_len);
+    //chlopaki nie maja czasu na bigendian 
+    uint8_t res[6 + tim_len];
+    // uint8_t res[8 + tim_len];
+    setArrayToResponse(res, rq_id, TIME_RESPONSE);
+    res[5] = (uint8_t)tim_len;
+    // memcpy(&res[5], &network_tim_len, sizeof(uint16_t));
+    // memcpy(&res[7], time_str, tim_len);
+    // write(client_socket, res, 7 + tim_len);
+    memcpy(&res[6], time_str, tim_len);
+    write(client_socket, res, 6 + tim_len);
+}
+
+void *client_thread(void *arg)
+{
+    Client_t *client = (Client_t *) arg;
+    int client_socket = client->socket_fd;
+    uint8_t buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+    while((bytes_read = read(client_socket, buffer, BUFFER_SIZE)) > 0)
+    {
+        size_t offset = 0;
+        //kilka zapytan na raz
+        while(offset < bytes_read)
+        {
+            uint8_t rq_id = buffer[offset + 4];
+            switch(buffer[offset + 3])
+            {
+                case SQRT_REQUEST:
+                    fprintf(stdout,"SQRT_REQUEST\n");
+                    sqrt_request(client_socket, rq_id, &buffer[offset + 5]);
+                    offset += 13;
+                    break;
+                case TIME_REQUEST:
+                    fprintf(stdout,"TIME_REQUEST\n");
+                    time_request(client_socket, rq_id);
+                    offset += 5;
+                    break;
+                default:
+                    printf("Invalid request\n");
+                    offset++;
+                    break;
+            }
+        }
+    }
+    close(client_socket);
+    free(client);
+    pthread_mutex_lock(&thread_count_mutex);
+    thread_count--;
+    pthread_mutex_unlock(&thread_count_mutex);
+    return NULL;
+}
+//dodac wybor karty sieciowe
+//timeout dla serwerow
+int main()
+{
+    setbuf(stdout, NULL);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    int server_socket;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+    pthread_t client_threads[MAX_CLIENT_THREADS];
+    //socket create
+    long int PORT;
+    parameters_read(&PORT);    
+    //ipv4, tcp, 0 -- default tcp
+    if((server_socket = socket(AF_INET, SOCK_STREAM, 0))==-1)
+    {
+        fprintf(stdout,"2. Socket creation failed\n");
+        return -1;
+    }
+    else
+    {
+        fprintf(stdout,"2. Socket created successfully\n");
+    }
+    int socket_option = 1;
+    //restart serwera zapobiega bleda
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR,&socket_option, sizeof(socket_option)) < 0) {
+        perror("3. Setsockopt failed\n");
+        close(server_socket);
+        return -1;
+    }
+    //ipv4
+    server_address.sin_family = AF_INET;
+    //dowolny adr
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+    //bind - przypisanie adresu do socketa
+    if(bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address))==-1)
+    {
+        fprintf(stdout,"4. Binding failed\n");
+        close(server_socket);
+        return -1;
+    }
+    else
+    {
+        fprintf(stdout,"4. Binding successful\n");
+    }
+    //listener na gniezdzie
+    if(listen(server_socket, MAX_CLIENT_THREADS)==-1)
+    {
+        fprintf(stdout,"5. Listening failed\n");
+        close(server_socket);
+        return -1;
+    }
+    else
+    {
+        fprintf(stdout,"5. Listening successful\n");
+    }
+    fprintf(stdout,"6. all set up\n");
+    fprintf(stdout,"--------Server is running--------\n");
+    fprintf(stdout,"Server is listening on port %d\n", PORT);
+    while(1)
+    {
+        fprintf(stdout,"Waiting for client connection...\n");
+        socklen_t client_address_len = sizeof(client_address);
+        int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len);
+        if(client_socket == -1)
+        {
+            fprintf(stdout,"Failed to accept client connection\n");
+            continue;
+        }
+        fprintf(stdout,"Client connected\n");
+        Client_t *client = (Client_t *)malloc(sizeof(Client_t));
+        client->socket_fd = client_socket;
+        client->client_address = client_address;
+        if(pthread_create(&client_threads[thread_count], NULL, client_thread, client) != 0)
+        {
+            fprintf(stdout,"Failed to create client thread\n");
+            close(client_socket);
+            free(client);
+        }
+        else
+        {
+            //odejmowanie wątkw.
+            pthread_mutex_lock(&thread_count_mutex);
+            thread_count++;
+            pthread_mutex_unlock(&thread_count_mutex);
+        }
+    }
+    close(server_socket);
+    return 0;
+}
